54135bbc1ae415c90a01d12e86922d7808b7a7dd
Abraham Juárez <abraham-aj@hotmail.com>
2021-11-08 13:02:11 -0600

    Nombre de Proyecto: Java 8.
    Descripción y/o Comentarios: Se agregan Ámbitos Lambda (Lambda Scopes) para una nueva sintaxis.

--- src/main/java/com/learning/java8/example/app/lambda/calculos/RealizarOperacion.java
+++ src/main/java/com/learning/java8/example/app/lambda/calculos/RealizarOperacion.java
@@ -7,7 +7,12 @@ public class RealizarOperacion {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(RealizarOperacion.class);
 
+
+    private static double promedio;
+
     public static void main(String[] args) {
+
+        final int puntosExtra = 2;
 
         // Using Java 7
         /*Operacion operacion = new Operacion() {
@@ -20,7 +25,15 @@ public class RealizarOperacion {
         // Using Java 8
         Operacion operacion = (double x, double y) -> (x+y)/2;
 
+        Operacion operacionConPuntos = (x, y) -> {
+            double califinacion = (x+y)/2;
+            promedio = califinacion + puntosExtra;
+
+            return promedio;
+        };
+
         LOGGER.info("Promedio : {}", operacion.calcularPromedio(7, 8));
+        LOGGER.info("Promedio Final : {}", operacionConPuntos.calcularPromedio(7, 8));
     }
 
 }
