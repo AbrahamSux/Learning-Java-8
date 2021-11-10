221c9f1b0f766c5573dfdb6b92f0459f9c5475cd
Abraham Juárez <abraham-aj@hotmail.com>
2021-11-10 16:21:49 -0600

    Nombre de Proyecto: Java 8.
    Descripción y/o Comentarios: Se agrega ejemplo del uso de RemoveIf para quitar de la lista los elementos que no queremos.

---
+++ src/main/java/com/learning/java8/example/app/lambda/collections/RemoveIfCollection.java
@@ -0,0 +1,81 @@
+package com.learning.java8.example.app.lambda.collections;
+
+import com.learning.java8.example.app.lambda.collections.dto.OperacionDTO;
+import com.learning.java8.example.app.lambda.collections.utils.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+public class RemoveIfCollection {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RemoveIfCollection.class);
+
+    private List<OperacionDTO> listaOperaciones;
+
+    // MAIN.
+    public static void main(String[] args) {
+        RemoveIfCollection tester = new RemoveIfCollection();
+
+        tester.procesar();
+    }
+
+
+
+    // MÉTODOS.
+
+    public void procesar() {
+        llenarLista();
+
+        // Using RemoveIf.
+        listaOperaciones = usingRemoveIf(listaOperaciones);
+
+
+        listaOperaciones.forEach(operacion -> LOGGER.info("Operación : {}", operacion.toString()));
+    }
+
+    public void llenarLista() {
+        listaOperaciones = new ArrayList<>();
+        listaOperaciones.addAll(Constants.listaOperaciones);
+    }
+
+    public List<OperacionDTO> usingRemoveIf(List<OperacionDTO> list) {
+
+        // Using Java 7: Option 1
+        /*List<OperacionDTO> listToRemoveOne = new ArrayList<>();
+        for (OperacionDTO dto : list) {
+            if (!Constants.IDS_OP_COMPRAS.contains(dto.getId())) {
+                listToRemoveOne.add(dto);
+            }
+        }
+        list.removeAll(listToRemoveOne);*/
+
+
+        // Using Java 7: Option 2
+        /*List<OperacionDTO> listToRemoveTwo = new ArrayList<>(list);
+        for (OperacionDTO dto : list) {
+            if (!Constants.IDS_OP_COMPRAS.contains(dto.getId())) {
+                listToRemoveTwo.remove(dto);
+            }
+        }
+        list = listToRemoveTwo;/
+
+
+        // Using Java 7: Option 3
+        /*Iterator<OperacionDTO> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            if (!Constants.IDS_OP_COMPRAS.contains(iterator.next().getId())) {
+                iterator.remove();
+            }
+        }*/
+
+
+        // Using Java 8: Lambda Expression
+        list.removeIf(element -> !Constants.IDS_OP_VENTAS.contains(element.getId()));
+
+        return list;
+    }
+
+}
--- src/main/java/com/learning/java8/example/app/lambda/collections/utils/Constants.java
+++ src/main/java/com/learning/java8/example/app/lambda/collections/utils/Constants.java
@@ -20,37 +20,28 @@ public final class Constants implements Serializable {
      * CONSTANTES.
      */
 
-    public static final Long ID_OP_CAJA_COBRO_VENTA_AL_PUBLICO = 2L;
-    public static final Long ID_OP_CAJA_COBRO_DE_VENTA_CON_BILLETE = 3L;
+    public static final Long ID_OP_CAJA_COBRO_VENTA_PUBLICA = 2L;
+    public static final Long ID_OP_CAJA_COBRO_VENTA_PRIVADA = 3L;
+    public static final Long ID_OP_CAJA_PAGO_COMPRA_PUBLICA = 6L;
+    public static final Long ID_OP_CAJA_PAGO_COMPRA_PRIVADA = 7L;
 
 
 
     public static final Set<Long> IDS_OP_VENTAS = asSet(
-            ID_OP_CAJA_COBRO_VENTA_AL_PUBLICO,
-            ID_OP_CAJA_COBRO_DE_VENTA_CON_BILLETE
+            ID_OP_CAJA_COBRO_VENTA_PUBLICA,
+            ID_OP_CAJA_COBRO_VENTA_PRIVADA
+    );
+
+    public static final Set<Long> IDS_OP_COMPRAS = asSet(
+            ID_OP_CAJA_PAGO_COMPRA_PUBLICA,
+            ID_OP_CAJA_PAGO_COMPRA_PRIVADA
     );
 
     public static List<OperacionDTO> listaOperaciones = Arrays.asList(
-            new OperacionDTO(1, "PP", "Pagar Prestamo"),
-            new OperacionDTO(2, "VP", "Venta al Publico"),
-            new OperacionDTO(3, "VB", "Venta con Billete"),
-            new OperacionDTO(4, "CV", "Venta con Billete para Empeno"),
-            new OperacionDTO(5, "QE", "Liquidacion por Reempeno"),
-            new OperacionDTO(6, "DS", "Cobro Desempeño"),
-            new OperacionDTO(7, "RP", "Recepcion Pago Referenciado"),
-            new OperacionDTO(8, "RF", "Cobro Refrendo"),
-            new OperacionDTO(9, "CI", "Cobro Reimpresion Billete"),
-            new OperacionDTO(10, "RB", "Cobro Reposicion Billete"),
-            new OperacionDTO(11, "BA", "Cobro de Cargo Anticip,ado a Valuador"),
-            new OperacionDTO(12, "MP", "Marcar como Separ,ada"),
-            new OperacionDTO(13, "DC", "Caducar Demasia,"),
-            new OperacionDTO(14, "PD", "Pagar Demasia"),
-            new OperacionDTO(15, "GA", "Cobro Almacenaje"),
-            new OperacionDTO(16, "AD", "Acreditacion de D,esempeno Banco"),
-            new OperacionDTO(17, "AS", "Servicio Avaluo"),
-            new OperacionDTO(18, "AX", "Acreditacion de Dese,mpeno Cruzado"),
-            new OperacionDTO(19, "BS", "Baja por Siniestro"),
-            new OperacionDTO(20, "PI", "Pago de Indemnizacion")
+            new OperacionDTO(2, "VPU", "Venta Pública"),
+            new OperacionDTO(3, "VPR", "Venta Privada"),
+            new OperacionDTO(6, "CPU", "Compra Pública"),
+            new OperacionDTO(7, "CPR", "Compra Privada")
     );
 
 }
