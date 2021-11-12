0364b01cec709105e63b7ba1e93c04be963ebc4d
Abraham Juárez <abraham-aj@hotmail.com>
2021-11-12 12:24:54 -0600

    Nombre de Proyecto: Java 8.
    Descripción y/o Comentarios: se agregan ejemplo para el uso de Optional (una 'lista de Optional' y un Optional con una lista).

--- src/main/java/com/learning/java8/example/app/optional/OptionalApp.java
+++ src/main/java/com/learning/java8/example/app/optional/OptionalApp.java
@@ -1,8 +1,13 @@
 package com.learning.java8.example.app.optional;
 
+import com.learning.java8.example.app.models.dto.OperacionDTO;
+import com.learning.java8.example.app.utils.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 public class OptionalApp {
@@ -17,23 +22,43 @@ public class OptionalApp {
         OptionalApp tester = new OptionalApp();
 
         // Optional con try-catch de manera tradicional
+        LOGGER.info(">>> Usando Optional.empty()... ");
         tester.process(null);
 
 
         // Usando OrElse
+        LOGGER.info(">>> Usando OrElse... ");
         tester.usingOrElse(null);
 
 
         // Usando OrElseThrow
+        LOGGER.info(">>> Usando OrElseThrow... ");
         /*tester.usingOrElseThrow(null);*/
         // or
         tester.usingOrElseThrow("Test...");
 
 
-        //Usando IfPresent
+        // Usando IfPresent
+        LOGGER.info(">>> Usando IfPresent... ");
         tester.usingIfPresent(null);
         // or
         tester.usingIfPresent("Element String");
+
+
+        // Usando List<Optional<Object>>
+        LOGGER.info(">>> Usando List<Optional<Object>>... ");
+        List<Optional<OperacionDTO>> listOptional = tester.usingListOptional();
+        if (listOptional != null && !listOptional.isEmpty()) {
+            LOGGER.info("Lista de Elementos Opcionales : {}", listOptional);
+        }
+
+
+        // Usando Optional<List<OperacionDTO>>
+        LOGGER.info(">>> Usando Optional<List<OperacionDTO>>... ");
+        Optional<List<OperacionDTO>> optionalList = tester.usingOptionalList();
+        if (optionalList.isPresent()) {
+            LOGGER.info("Optional List : {}", optionalList);
+        }
     }
 
 
@@ -68,6 +93,55 @@ public class OptionalApp {
         Optional<String> optional = Optional.ofNullable(value);
 
         LOGGER.info("Existe Elemento ? : {} : {}", optional.isPresent(), optional.orElse("NULL"));
+    }
+
+    /**
+     * Ejemplo del uso de una Lista de Optional.
+     *
+     * @return List<Optional<OperacionDTO>>
+     */
+    public List<Optional<OperacionDTO>> usingListOptional() {
+
+        List<OperacionDTO> listaOperaciones = new ArrayList<>(Constants.listOfOperations);
+        List<Optional<OperacionDTO>> optional = new ArrayList<>();
+
+        // Lista Normal
+        listaOperaciones.stream().sorted(Comparator.comparing(OperacionDTO::getId).reversed()).forEach( e -> {
+            // LOGGER.info("Elemento Normal : {}", e);
+
+            optional.add(Optional.of(e));
+        });
+
+        // Lista de Optional
+        if (!optional.isEmpty()) {
+            optional.forEach( e -> LOGGER.info("Element Optional : {}", e));
+        }
+
+        return optional;
+    }
+
+    /**
+     * Ejemplo del uso de un Optional con una Lista.
+     *
+     * @return Optional<List<OperacionDTO>>
+     */
+    public Optional<List<OperacionDTO>> usingOptionalList() {
+
+        List<OperacionDTO> listaOperaciones = new ArrayList<>(Constants.listOfOperations);
+        Optional<List<OperacionDTO>> optionalList = Optional.empty();
+
+        // Lista Normal
+        listaOperaciones.stream().sorted(Comparator.comparing(OperacionDTO::getId).reversed()).forEach( e -> {
+            LOGGER.info("Elemento Normal : {}", e);
+        });
+
+        optionalList = Optional.of(listaOperaciones);
+
+        optionalList.ifPresent(lista -> lista.forEach( e ->
+                LOGGER.info("Element Optional : {}", e)
+        ));
+
+        return optionalList;
     }
 
 }
